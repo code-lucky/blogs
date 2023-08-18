c613aef963170a4d7131f53e496f956f868388b6
huangqh <2924249993@qq.com>
2023-08-18 16:27:17 +0800

    uniapp做微信小程序的时候web-view导航栏返回问题

---
+++ docs/.vuepress/vue-previews/jssdk.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div @click="test"></div>
+    </div>
+</template>
+
+<script>
+export default{
+    methods:{
+        test(){
+            // 可以先打印一下，如果引入成功可以成功打印出来
+            console.log(wx)
+
+            // 所有的路由方法都在 miniProgram 下
+            wx.miniProgram.redirectTo({
+                url:'/pages/index/index'
+            })
+        }
+    }
+}
+</script>
