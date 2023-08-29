c7378200ad62f041108642ff6e37bb4aa60e55ae
huangqh <2924249993@qq.com>
2023-08-29 14:29:46 +0800

    新增linux部署nestjs项目blog

---
+++ docs/.vuepress/vue-previews/nest-build-pm2.vue
@@ -0,0 +1,14 @@
+// 1、打包
+npm run build
+//2、创建
+在宝塔文件"/www/wwwroot/"下创建一个文件,将打包的服务器代码放进去
+//3、打开pm2管理器，添加你的项目
+
+如果管理器启动项目失败，请使用以下手动启动
+
+$ cd /www/wwwroot/nest
+$ cnpm install
+$ npm install
+$ npm run build
+$ node dist/main.js // 先测试运行是否有错误
+$ pm2 start npm --name nest -- run start // pm2管理项目，直接在服务端跑起来
