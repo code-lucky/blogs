f7fc4dfa94a9d076c981575f59919d5fe7b006c0
huangqh <2924249993@qq.com>
2023-11-03 16:45:21 +0800

    v-show和v-if有什么区别？使用场景分别是什么？

---
+++ docs/.vuepress/vue-previews/show-if.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="continer">
+    <div v-if="isShow">v-if</div>
+    <div v-show="isShow">v-show</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      isShow: true
+    }
+  }
+}
+</script>
